2b5a65a9839967ebd3cd62a1e1bec28f36911b8c
literayno4 <literaryno4@gmail.com>
2023-06-27 16:54:27 +0800

    add compile_commands.json

--- value.h
+++ value.h
@@ -26,7 +26,7 @@ typedef struct {
 
 #define BOOL_VAL(value)   ((Value){VAL_BOOL, {.boolean = value}})
 #define NIL_VAL           ((Value){VAL_NIL, {.number = 0}})
-#define NUMBER_VAL(value) ((Value){VAL_BOOL, {.boolean = value}})
+#define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = value}})
 
 typedef struct {
   int capacity;
@@ -34,6 +34,7 @@ typedef struct {
   Value* values;
 } ValueArray;
 
+bool valuesEqual(Value a, Value b);
 void initValueArray(ValueArray* array);
 void writeValueArray(ValueArray* array, Value value);
 void freeValueArray(ValueArray* array);
